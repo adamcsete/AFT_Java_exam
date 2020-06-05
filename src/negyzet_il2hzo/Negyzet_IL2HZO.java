92a0817a9dc991bc0137779fb0d7f9c96ce8ca2e
Csete Adam <cseteadam91@gmail.com>
2020-06-05 16:45:49 +0200

    4zetelés

--- src/negyzet_il2hzo/Negyzet_IL2HZO.java
+++ src/negyzet_il2hzo/Negyzet_IL2HZO.java
@@ -5,17 +5,81 @@
  */
 package negyzet_il2hzo;
 
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.GridLayout;
+import java.awt.Toolkit;
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 /**
  *
  * @author jani
  */
 public class Negyzet_IL2HZO {
-
+    
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        int width = Toolkit.getDefaultToolkit().getScreenSize().width;
+        int height = Toolkit.getDefaultToolkit().getScreenSize().height;
+        JFrame frame =  new JFrame("Negyzetelés");
+        JButton MButton0 = new JButton("MButton0");
+        MButton0.setBackground(Color.BLUE);
+        JButton MButtonP = new JButton("MButtonP");
+        MButtonP.setBackground(Color.YELLOW);
+        JButton GoGomb = new JButton("GoGomb");
+        GoGomb.setBackground(Color.MAGENTA);
+        
+        JOptionPane pane = new JOptionPane();
+        JLabel label1 = new JLabel();
+        JLabel label2 = new JLabel();
+        JLabel label3 = new JLabel("5");
+        
+        frame.setLayout(new GridLayout(2, 3));
+        frame.add(label1);
+        frame.add(label2);
+        frame.add(label3);
+        frame.add(MButton0);
+        frame.add(MButtonP);
+        frame.add(GoGomb);
+        frame.pack();
+        
+        
+        frame.setLocation(width/4, height/4);
+        frame.setSize(new Dimension(width/4,height/4));
+        frame.setVisible(true);
+        frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        
+        MButton0.addActionListener(new ActionListener() {
+                                             @Override
+                                             public void actionPerformed(ActionEvent e) {
+                                                 int valt = Integer.parseInt(label3.getText());
+                                                 if(valt>3) valt--;
+                                                 label3.setText(Integer.toString(valt));
+                                             }
+                                         });
+        
+        
+        MButtonP.addActionListener(new ActionListener() {
+                                             @Override
+                                             public void actionPerformed(ActionEvent e) {
+                                                 int valt = Integer.parseInt(label3.getText());
+                                                 if(valt<7) valt++;
+                                                 label3.setText(Integer.toString(valt));
+                                             }
+                                         });
+        
+        GoGomb.addActionListener(new ActionListener() {
+                                             @Override
+                                             public void actionPerformed(ActionEvent e) {
+                                                 int eredmeny = negyzet(Integer.parseInt(label3.getText())); 
+                                                 JOptionPane.showConfirmDialog(frame, label3.getText() + " négyzete: " + Integer.toString(eredmeny), "Négyzet", JOptionPane.DEFAULT_OPTION,JOptionPane.PLAIN_MESSAGE);
+                                             }
+                                         });
+        
     }
     
     public static int negyzet(int Bemenet){
